7970deeeedfedf8c460ed72b8ac059c5df688b02
Volodymyr <volodyaruzhak@gmail.com>
2023-01-21 13:13:04 -0500

    add new endpoint for getSongByGenre and modified a discover page to make it work properly (now genre can be selected and displayed aproprietly)

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,19 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// next content could ne useless - but for know Ill keep it
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': '7f0834bd79msh480b4f1ef5f113dp16b8a6jsna92d72f2b089',
-//         'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
-//     },
-// };
-
-// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-//     .then((response) => response.json())
-//     .then((response) => console.log(response))
-//     .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
@@ -26,6 +12,7 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
         getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
         getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
@@ -35,6 +22,7 @@ export const shazamCoreApi = createApi({
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
